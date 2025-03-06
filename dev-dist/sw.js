b157f7567aba8b79916a07d3608f86d6ea119489
fergus-g <fergus.gildea@utrack.com>
2025-03-06 19:54:11 +0000

    Added opacity to background image

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,11 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-
-
-    "revision": "0.85iu3ntdguo"
-
-
+    "revision": "0.4q7cdenct8o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
